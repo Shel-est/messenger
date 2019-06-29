5593f4b86f32051d98187382d3ee957dd56beb05
shel-est <sheletandr@gmail.com>
2019-06-29 10:42:17 +0700

    Complete OAuth2; Add webpack, node.js, yarn, refactor

--- src/main/java/com/example/messenger/domain/User.java
+++ src/main/java/com/example/messenger/domain/User.java
@@ -1,13 +1,16 @@
 package com.example.messenger.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usr")
-public class User {
+public class User implements Serializable {
     @Id
     private String id;
     private String name;
@@ -15,6 +18,7 @@ public class User {
     private String email;
     private String gender;
     private String locale;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy HH:mm:ss")
     private LocalDateTime lastVisit;
 
     public String getId() {
